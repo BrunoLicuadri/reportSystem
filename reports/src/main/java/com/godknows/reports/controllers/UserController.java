6f0ca62e095588d5e1dad61820d3182d21d96009
blicuadri <blicuadri@gmail.com>
2024-03-17 17:08:03 -0300

    Delivery section

--- reports/src/main/java/com/godknows/reports/controllers/UserController.java
+++ reports/src/main/java/com/godknows/reports/controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.godknows.reports.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.godknows.reports.dtos.UserDTO;
+import com.godknows.reports.entities.Role;
 import com.godknows.reports.services.UserService;
 
 
@@ -25,5 +31,13 @@ public class UserController {
 	public ResponseEntity<UserDTO> getMe() {
 		UserDTO dto = service.getme();
 		return ResponseEntity.ok(dto);
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+	@PostMapping(value="/newuser")
+	public ResponseEntity<UserDTO> newUser(@RequestBody UserDTO dto, @RequestBody Role role){
+		dto = service.insertUser(dto, role);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{users}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- reports/src/main/java/com/godknows/reports/services/UserService.java
+++ reports/src/main/java/com/godknows/reports/services/UserService.java
@@ -1,6 +1,7 @@
 package com.godknows.reports.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.godknows.reports.dtos.UserDTO;
+import com.godknows.reports.entities.Role;
 import com.godknows.reports.entities.User;
 import com.godknows.reports.repositories.UserRepository;
 
@@ -17,6 +19,7 @@ public class UserService implements UserDetailsService {
 
 	@Autowired
 	public UserRepository repository;
+
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -42,5 +45,31 @@ public class UserService implements UserDetailsService {
 	public UserDTO getme() {
 		User entity  = authenticated();
 		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insertUser (UserDTO dto, Role role) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		entity.setPassword("123456");
+		entity.addRole(role);
+		return new UserDTO(entity);
+	}
+	
+	
+	private void copyDtoToEntity (UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setEmail(dto.getEmail());
+		entity.setPhone(dto.getPhone());
+
+		for(String authority : dto.g) {
+			Role role = new Role();
+			role.setId(authority.getAuthority());
+			entity.getRoles().add(role);
+		}
+		
+		
 	}
 }
