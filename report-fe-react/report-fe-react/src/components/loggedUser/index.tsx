d2692290e9c1c1d11b1943eb954b0a01012255ba
blicuadri <blicuadri@gmail.com>
2024-05-02 12:24:14 -0300

    FE - custom inputForm

--- report-fe-react/report-fe-react/src/components/loggedUser/index.tsx
+++ report-fe-react/report-fe-react/src/components/loggedUser/index.tsx
@@ -4,19 +4,36 @@ import { CredentialsDTO } from '../../models/auth';
 import * as authService from '../../services/authService';
 import { ContextToken } from '../../utils/contextTokenPayload';
 import './styles.css';
+import FormInput from '../formInput';
 
 export default function LoggedUser() {
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: "",
-        password: ""
+    const [formData, setFormData] = useState<any>({
+        username: {
+            value: "",
+            id: "username",
+            name: "username",
+            type: "text",
+            placeholder: "Email",
+            validation: function (value: string) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+            },
+            message: "Favor informar um usuário válido",
+        },
+        password: {
+            value: "",
+            id: "password",
+            name: "password",
+            type: "password",
+            placeholder: "Senha",
+        }
     })
 
     const navigate = useNavigate();
 
     const { contextTokenPayload, setContextTokenPayload } = useContext(ContextToken);
 
-    
+
     function handleClick() {
         authService.logout();
         setContextTokenPayload(undefined);
@@ -26,13 +43,10 @@ export default function LoggedUser() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        authService.loginRequest(formData)
+        authService.loginRequest({username: formData.username.value, password: formData.password.value})
             .then((response) => {
                 authService.saveAccessToken(response.data.access_token);
                 setContextTokenPayload(authService.getAccessTokenPayload());
-                //navigate("/reports")
-                console.log(authService.getAccessTokenPayload());
-                console.log({contextTokenPayload});
             })
             .catch((error) => {
                 console.log("erro no login", error)
@@ -42,7 +56,7 @@ export default function LoggedUser() {
     function handleInputChange(event: any) {
         const value = event.target.value;
         const name = event.target.name;
-        setFormData({ ...formData, [name]: value });
+        setFormData({ ...formData, [name]: {...formData[name], value: value} });
     }
 
     return (
@@ -58,25 +72,17 @@ export default function LoggedUser() {
                     <h2 className="loginTextClass">Login</h2>
                     <div className="loginInputClass">
                         <div>
-                            <input
-                                name="username"
-                                value={formData.username}
-                                id="inputName"
+                            <FormInput
+                                {...formData.username}
                                 className="inputClassName"
-                                type="text"
-                                placeholder="Nome"
                                 onChange={handleInputChange}
                             />
                             <div className="error-message" data-error="name"></div>
                         </div>
                         <div>
-                            <input
-                                name="password"
-                                value={formData.password}
-                                id="inputPassword"
+                            <FormInput
+                                {...formData.password}
                                 className="inputClassPassword"
-                                type="password"
-                                placeholder="Senha"
                                 onChange={handleInputChange}
                             />
                             <div className="error-message" data-error="password"></div>
